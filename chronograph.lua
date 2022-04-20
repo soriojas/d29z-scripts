489fe0394b1aad78b61b8f8b0da42eebeb5427c8
soriojas <32626362+soriojas@users.noreply.github.com>
2022-04-20 10:50:22 -0400

    Update chronograph.lua

--- chronograph.lua
+++ chronograph.lua
@@ -1,2 +1,12 @@
---chronograph
-
+local Sign = GetPartFromPort(1, "Sign")
+GetPort(1):Connect("Triggered", function()
+    local initialTick = tick()
+    Sign:Configure({SignText="--"})
+    while wait() do
+        local Glass = GetPartFromPort(2, "Glass")
+        if Glass == nil then
+            Sign:Configure({SignText=tick()-initialTick})
+            break
+        end
+    end
+end)
