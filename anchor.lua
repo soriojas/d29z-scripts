55f6a283155dc076f721705aad5d2ca7cff83de7
soriojas <32626362+soriojas@users.noreply.github.com>
2022-08-26 18:50:13 -0400

    Update anchor.lua

--- anchor.lua
+++ anchor.lua
@@ -1,19 +1,20 @@
 local Microphone = GetPartFromPort(1, "Microphone") -- Gets microphone attached to port 1
 local Anchor = GetPartFromPort(1, "Anchor") -- Gets anchor attached to port 1
-local Owner = "namehere"
+local Owner = "playerName"
 local Enabled = false
 
 -- Connects to the microphone, adding an event to it that will listen for chat input
 Microphone:Connect("Chatted", function(Player, Message) 
-    if Player == Owner then return end
-    if Message:lower():sub(0, 6) == "anchor" then -- If the message starts with anchor
+    if Player ~= Owner then return end
+    if Message:lower():sub(0, 6) == "/anchor" then -- If the message starts with anchor
         Enabled = true
         Anchor:Configure({Anchored=true})
-    elseif Message:lower():sub(0, 6) == "unanchor" then 
+    elseif Message:lower():sub(0, 8) == "/unanchor" then
         Enabled = false
     end
-    while not Enabled do --repeatedly disable the anchor
-        Anchor:Configure({Anchored=false})
-        wait(2)
-    end
 end)
+
+while Enabled == false do --repeatedly disable the anchor to prevent to prevent leaving it on by accident
+    Anchor:Configure({Anchored=false})
+    wait(4)
+end
