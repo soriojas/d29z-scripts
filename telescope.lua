820a22298ffbed22e48456b7ff3d9cb4c9832697
soriojas <32626362+soriojas@users.noreply.github.com>
2024-10-01 09:26:40 -0400

    Update telescope.lua

--- telescope.lua
+++ telescope.lua
@@ -66,5 +66,24 @@ print("End Script")
 1,-74,-2,7
 ]]
 
+function dumpTable(table)
+    local temp = ""
+    for key, value in pairs(table) do
+        print(type(value))
+        if type(value) == table then
+            temp = temp..dumpTable(value)
+        else
+            temp = temp..", "..key..":"..tostring(value)
+        end
+    end
+    return temp
+end
 
+function callback(regionData)
+    print(dumpTable(regionData))
+end
 
+while wait(0.333) do
+    local Telescope = GetPartFromPort(1, "Telescope")
+    Telescope:WhenRegionLoads(callback)
+end
