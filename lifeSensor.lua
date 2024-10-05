d56af389d357d61268516579101291be763ec56b
soriojas <32626362+soriojas@users.noreply.github.com>
2024-10-05 17:06:44 -0400

    rework lifeSensor.lua

--- lifeSensor.lua
+++ lifeSensor.lua
@@ -1,39 +1,26 @@
-local Screen = GetPartFromPort(1, 'Screen') -- assigning the screen on port 1 to a variable
+local Screen = GetPartFromPort(1, 'Screen')
 local LifeSensor = GetPartFromPort(2, 'LifeSensor')
-local Instrument = GetPartFromPort(3, 'Instrument')
-local scale = 0.05
-local screenSize = Screen:GetDimensions()
+local myPosition = LifeSensor.CFrame.Position
+local defaultLabelProperties = {        
+    AnchorPoint = Vector2.new(0.5, 0.5);
+    Position = UDim2.new({0.5, 0, 0.5, 0});
+    BackgroundTransparency = 1;
+    Size = UDim2.new(0,100,0,100);
+    Text = '+';
+    TextSize = 20;
+    TextScaled = false;
+    TextColor3 = Color3.new(1, 1, 10);
+}
 
-local myPosition = Instrument:GetReading()
-local myPosition = myPosition:gsub("%s+", "") --space remover
-local x, y, z = myPosition:match("([^,]+),([^,]+),([^,]+)")
-
-Screen:ClearElements() -- clear all elements already on the screen so there arent any old elements on it
-    Screen:CreateElement('TextLabel', {        
-        AnchorPoint = Vector2.new(0.5, 0.5);
-        BackgroundTransparency = 1;
-        Position = UDim2.new(0.5, x * scale * screenSize.x/1000 , 0.5, z  * scale * screenSize.y/1000);
-        Size = UDim2.new(0,100,0,100);
-        Text = '+';
-        TextSize = 20;
-        TextScaled = false;
-        TextColor3 = Color3.new(1, 1, 10);
-})
+Screen:ClearElements()
+local centerLabel = Screen:CreateElement('TextLabel', defaultLabelProperties)
+centerLabel.Text = "+"
 
 local reading = LifeSensor:GetReading()
-for playerName, position in pairs(reading) do
-    Screen:CreateElement('TextLabel', {
-        AnchorPoint = Vector2.new(0.5, 0.5);
-        BackgroundTransparency = 1;
-        Position = UDim2.new(0.5, position.x * scale * screenSize.x/1000, 0.5, position.z * scale * screenSize.y/1000 );
-        Size = UDim2.new(0,100,0,100);
-        Text = string.sub(playerName, 1, 2);
-        TextSize = 20;
-        TextScaled = false;
-        TextColor3 = Color3.new(1, 0, 0);
-    })
+local playerCache = {}
+local playerLabels = {}
+for playerName, playerPosition in pairs(reading) do
+    local screenSize = Screen:GetDimensions()
+    playerLabels[playerName] = Screen:CreateElement('TextLabel', defaultLabelProperties)
+    playerLabels[playerName] = UDim2.new(0.5, playerPosition.x*screenSize.x/1000, 0.5, playerPosition.z*screenSize.y/1000)
 end
-
---0.05 scale when 20x20 studs or 1000x1000px
-
-
