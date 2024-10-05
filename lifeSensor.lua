a8dd9a49ba9d9ff3de648f060778692fda2b516e
soriojas <32626362+soriojas@users.noreply.github.com>
2024-10-05 18:19:03 -0400

    Update lifeSensor.lua

--- lifeSensor.lua
+++ lifeSensor.lua
@@ -12,15 +12,30 @@ local defaultLabelProperties = {
     TextColor3 = Color3.new(1, 1, 10);
 }
 
-Screen:ClearElements()
-local centerLabel = Screen:CreateElement('TextLabel', defaultLabelProperties)
-centerLabel.Text = "+"
+Screen:ClearElements() --wipe the screen clean
 
-local reading = LifeSensor:GetReading()
-local playerCache = {}
-local playerLabels = {}
-for playerName, playerPosition in pairs(reading) do
-    local screenSize = Screen:GetDimensions()
-    playerLabels[playerName] = Screen:CreateElement('TextLabel', defaultLabelProperties)
-    playerLabels[playerName] = UDim2.new(0.5, playerPosition.x*screenSize.x/1000, 0.5, playerPosition.z*screenSize.y/1000)
+local centerLabel = Screen:CreateElement('TextLabel', defaultLabelProperties)
+centerLabel.Text = "+"  --draw the center
+
+local function movePlayerLabel(label)
+    label.position = UDim2.new(0.5, playerPosition.x*screenSize.x/1000, 0.5, playerPosition.z*screenSize.y/1000)
 end
+
+local playerLabels = {}
+local function updateReadings()
+    local screenSize = Screen:GetDimensions()
+    local reading = LifeSensor:GetReading()
+    for playerName, playerPosition in pairs(reading) do
+        if playerLabels[playerName] == nil then --create new label for new player
+            playerLabels[playerName] = Screen:CreateElement('TextLabel', defaultLabelProperties)
+        end
+        movePlayerLabel(playerLabels[playerName])
+        reading.remove(playerName) --remove scanned players from cache
+    end
+    for playerName, playerPosition in pairs(reading) do --delete labels from nonexistant players 
+        playerLabels[playerName]:Destroy()
+        playerLabels.remove(playerName)
+    end
+end
+
+GetPort(1):Connect("Triggered", updateReadings)
