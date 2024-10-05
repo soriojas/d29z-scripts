d99e1a20f6f938675a1c6d5e919b51cbd97e716e
soriojas <32626362+soriojas@users.noreply.github.com>
2024-10-05 16:39:37 -0400

    Update screen.lua

--- screen.lua
+++ screen.lua
@@ -1,22 +1,20 @@
 local screen = GetPartFromPort(1, "Screen")
 local screenSize = screen:GetDimensions()
-scale = 1
-print(screenSize.x)
 
 local textBox = screen:CreateElement('TextLabel', {        
-    AnchorPoint = Vector2.new(0.5, 0.5);
+    AnchorPoint = Vector2.new(0, 0);
     BackgroundTransparency = 0;
-    Position = UDim2.new(0.5, x * scale * screenSize.x , 0.5, z  * scale * screenSize.y);
-    Size = UDim2.new(0,100,0,100);
-    Text = "Hello world!";
+    Position = UDim2.new(0, 50, 0, 0);
+    Size = UDim2.new(0,50,0,50); -- 1 stud is 50px
+    Text = "Hello!";
     TextSize = 20;
     TextScaled = false;
     TextColor3 = Color3.new(1, 1, 10);
-})
+}) --see https://robloxapi.github.io/ref/class/TextLabel.html
 
 wait(2)
 
-textBox.Text = "Goodbye world."
+textBox.Text = "Bye."
 
 wait(2)
 
