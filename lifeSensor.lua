7012538bbd4d3223aa0a5a762262ec1b08ac5ac5
soriojas <32626362+soriojas@users.noreply.github.com>
2024-10-05 21:41:48 -0400

    Update lifeSensor.lua

--- lifeSensor.lua
+++ lifeSensor.lua
@@ -1,9 +1,11 @@
-local Screen = GetPartFromPort(1, 'Screen')
-local LifeSensor = GetPartFromPort(2, 'LifeSensor')
-local myPosition = LifeSensor.CFrame.Position
+--incomplete
+
+local Screen = GetPartFromPort(1, 'Screen') print(Screen)
+local LifeSensor = GetPartFromPort(1, 'LifeSensor') print(LifeSensor)
+--local myPosition = LifeSensor.Position
 local defaultLabelProperties = {        
     AnchorPoint = Vector2.new(0.5, 0.5);
-    Position = UDim2.new({0.5, 0, 0.5, 0});
+    Position = UDim2.new(0.5, 0, 0.5, 0);
     BackgroundTransparency = 1;
     Size = UDim2.new(0,100,0,100);
     Text = '+';
@@ -15,9 +17,8 @@ local defaultLabelProperties = {
 Screen:ClearElements() --wipe the screen clean
 
 local centerLabel = Screen:CreateElement('TextLabel', defaultLabelProperties)
-centerLabel.Text = "+"  --draw the center
 
-local function movePlayerLabel(label)
+local function movePlayerLabel(label, position) --todo: make label pos relative to player pos
     label.position = UDim2.new(0.5, playerPosition.x*screenSize.x/1000, 0.5, playerPosition.z*screenSize.y/1000)
 end
 
@@ -25,17 +26,27 @@ local playerLabels = {}
 local function updateReadings()
     local screenSize = Screen:GetDimensions()
     local reading = LifeSensor:GetReading()
-    for playerName, playerPosition in pairs(reading) do
-        if playerLabels[playerName] == nil then --create new label for new player
-            playerLabels[playerName] = Screen:CreateElement('TextLabel', defaultLabelProperties)
+    if #reading >= #playerLabels then
+        for name, position in pairs(reading) do
+            if playerLabels[name] == nil then
+                playerLabels[name] = Screen:CreateElement('TextLabel', defaultLabelProperties)
+            end
+            movePlayerLabel(playerLabels[name], position)
         end
-        movePlayerLabel(playerLabels[playerName])
-        reading.remove(playerName) --remove scanned players from cache
-    end
-    for playerName, playerPosition in pairs(reading) do --delete labels from nonexistant players 
-        playerLabels[playerName]:Destroy()
-        playerLabels.remove(playerName)
+    else
+        for name, label in playerLabels do
+            if reading[name] == nil then
+                label:Destroy()
+                playerLabels.remove(name)
+            end
+        end
     end
 end
 
 GetPort(1):Connect("Triggered", updateReadings)
+
+
+-- for playerName, playerPosition in pairs(reading) do --delete labels from nonexistant players 
+--     playerLabels[playerName]:Destroy()
+--     playerLabels.remove(playerName)
+-- end
