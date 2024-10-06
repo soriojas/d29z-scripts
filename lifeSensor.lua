dfe2c3078759282bda597d6d63a60dd5a253bfdd
soriojas <32626362+soriojas@users.noreply.github.com>
2024-10-06 16:43:35 -0400

    updated lifeSensor

--- lifeSensor.lua
+++ lifeSensor.lua
@@ -2,7 +2,7 @@
 
 local Screen = GetPartFromPort(1, 'Screen') print(Screen)
 local LifeSensor = GetPartFromPort(1, 'LifeSensor') print(LifeSensor)
---local myPosition = LifeSensor.Position
+local myPosition = LifeSensor.Position
 local defaultLabelProperties = {        
     AnchorPoint = Vector2.new(0.5, 0.5);
     Position = UDim2.new(0.5, 0, 0.5, 0);
@@ -18,20 +18,25 @@ Screen:ClearElements() --wipe the screen clean
 
 local centerLabel = Screen:CreateElement('TextLabel', defaultLabelProperties)
 
-local function movePlayerLabel(label, position) --todo: make label pos relative to player pos
-    label.position = UDim2.new(0.5, playerPosition.x*screenSize.x/1000, 0.5, playerPosition.z*screenSize.y/1000)
+--todo: make label pos relative to player pos
+local function movePlayerLabel(label, position)
+    local scale = 0.1
+    label.Position = UDim2.new(0.5, position.x * scale, 0.5, position.z * scale);
 end
 
 local playerLabels = {}
-local function updateReadings()
+local function update()
+    myPosition = LifeSensor.Position
     local screenSize = Screen:GetDimensions()
     local reading = LifeSensor:GetReading()
     if #reading >= #playerLabels then
         for name, position in pairs(reading) do
             if playerLabels[name] == nil then
                 playerLabels[name] = Screen:CreateElement('TextLabel', defaultLabelProperties)
+                playerLabels[name].Text = string.sub(name, 1, 3)
             end
             movePlayerLabel(playerLabels[name], position)
+            print(name.." "..position.x.." "..position.y.." "..position.z)
         end
     else
         for name, label in playerLabels do
@@ -43,10 +48,4 @@ local function updateReadings()
     end
 end
 
-GetPort(1):Connect("Triggered", updateReadings)
-
-
--- for playerName, playerPosition in pairs(reading) do --delete labels from nonexistant players 
---     playerLabels[playerName]:Destroy()
---     playerLabels.remove(playerName)
--- end
+GetPort(1):Connect("Triggered", update)
